83578d420ca969c7c562382ad41a7b7e9c13846d
Adrien Abbey <abbey.12@wright.edu>
2023-03-31 10:07:20 -0400

    created basic outline from header declarations

---
+++ Trie.cpp
@@ -0,0 +1,51 @@
+// Adrien Abbey, CS-3100, Mar. 30, 2023
+// Project #5: Autocomplete with an Alphabet Trie
+// VS Code, Ubuntu 22.04, g++/gdb
+
+#include <string>
+#include "Trie.h"
+
+using namespace std;
+
+/* Constructors */
+
+Trie::Trie()
+{
+    // test
+}
+
+Trie::Trie(const Trie &other)
+{
+}
+
+/* Methods */
+
+bool Trie::insert(string word)
+{
+}
+
+int Trie::count()
+{
+}
+
+int Trie::getSize()
+{
+}
+
+bool Trie::find(string word)
+{
+}
+
+int Trie::completeCount(string partialWord)
+{
+}
+
+vector<string> Trie::complete(string partialWord)
+{
+}
+
+/* Overrides */
+
+Trie &Trie::operator=(const Trie &other)
+{
+}
---
+++ TrieNode.cpp
@@ -0,0 +1,14 @@
+// Adrien Abbey, CS-3100, Mar. 30, 2023
+// Project #5: Autocomplete with an Alphabet Trie
+// VS Code, Ubuntu 22.04, g++/gdb
+
+#include <string>
+#include "TrieNode.h"
+
+using namespace std;
+
+/* Constructors */
+
+TrieNode::TrieNode()
+{
+}
