dd3db0fe9f8d338b4b2796d7b7eebc9bea9752a3
Adrien Abbey <abbey.12@wright.edu>
2023-03-31 09:59:05 -0400

    copied Makefile from OneNote, basic main class

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// Adrien Abbey, CS-3100, Mar. 30, 2023
+// Project #5: Autocomplete with an Alphabet Trie
+// VS Code, Ubuntu 22.04, g++/gdb
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
