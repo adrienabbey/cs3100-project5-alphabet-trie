f952ea07ee778cfd2af94a68a2d5ad6f8bbd83a6
Adrien Abbey <abbey.12@wright.edu>
2023-03-31 11:06:01 -0400

    clarify description, add findChild method

--- TrieNode.h
+++ TrieNode.h
@@ -14,9 +14,8 @@ private:
     /* Fields */
 
     /// @brief A fixed-sized array of pointers to other nodes.  Each index
-    /// represents a letter of the alphabet (i = 0 for 'a', i = 1 for 'b',
-    /// etc.).  An additional index is added to represent the terminating
-    /// character (i = 26 for '&').
+    /// represents a letter of the alphabet (i = 1 for 'a', i = 26 for 'z',
+    /// etc.).  Index 0 represents the terminating character (i = 0 for '&').
     TrieNode *pointerArray[27];
 
 public:
@@ -24,4 +23,11 @@ public:
 
     /// @brief Creates an empty trie node.
     TrieNode();
+
+    /* Methods */
+
+    /// @brief Checks to see if a child node exists for the given character, returning a reference to that node if it exists.
+    /// @param character The character to search for.
+    /// @return Returns a reference to a Trie Node if one exists for the given character.
+    TrieNode *findChild(const char &character);
 };
