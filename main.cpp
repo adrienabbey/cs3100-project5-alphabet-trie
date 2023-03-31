2ff5cb1d44279da27357921f5c69b183919d0773
Adrien Abbey <abbey.12@wright.edu>
2023-03-31 19:11:42 -0400

    add notes, tested trie constructor, insert method

--- main.cpp
+++ main.cpp
@@ -2,7 +2,16 @@
 // Project #5: Autocomplete with an Alphabet Trie
 // VS Code, Ubuntu 22.04, g++/gdb
 
+/*
+ * Notes:
+ *  - Since we are provided dictionaries to work with, I'm going to assume
+ *    that we're ONLY working with 26 lowercase letters plus a terminating
+ *    'character'.  This means not only no capital letters, but also no
+ *    punctuation or other characters.
+ */
+
 #include <iostream>
+#include "Trie.h"
 
 using namespace std;
 
@@ -12,6 +21,12 @@ int main()
     char testChar = 'a';
     int charInt = testChar - 'a' + 1;
     cout << "Character " << testChar << "'s int value is: " << charInt << endl;
+
+    // Create an empty trie:
+    Trie *testTrie = new Trie();
+
+    // Insert test:
+    testTrie->insert("hello");
 
     // Load the appropriate dictionary into a trie:
     // NOTE: See OneNote!
