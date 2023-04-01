9b312c0ed793dfdedbbef022b913d73639f33392
Adrien Abbey <abbey.12@wright.edu>
2023-04-01 19:58:26 -0400

    impl. completeHelper method

--- TrieNode.cpp
+++ TrieNode.cpp
@@ -3,6 +3,7 @@
 // VS Code, Ubuntu 22.04, g++/gdb
 
 #include <string>
+#include <vector>
 #include "TrieNode.h"
 
 using namespace std;
@@ -91,5 +92,34 @@ bool TrieNode::newChild(const char &character)
     {
         // A node already exists, do not create a new node, return false:
         return false;
+    }
+}
+
+void TrieNode::completeHelper(string partialWord, vector<string> &stringVector)
+{
+    // This is a recursive function.
+
+    // If this node has a terminator node:
+    if (pointerArray[0] != nullptr)
+    {
+        // Then add the current partial word to the string vector:
+        stringVector.push_back(partialWord);
+    }
+
+    // For each child (excluding terminator node):
+    for (int i = 1; i < 27; i++)
+    {
+        // If the child exists:
+        if (pointerArray[i] != nullptr)
+        {
+            // Add the node's character value to a string:
+            char letter = 'a' + i - 1;
+            string newString = partialWord; // https://stackoverflow.com/a/4751624
+            newString += letter;            // https://stackoverflow.com/a/1472075
+
+            // Then recursively call this function on the child with the given:
+            TrieNode *childNode = pointerArray[i];
+            childNode->completeHelper(newString, stringVector);
+        }
     }
 }
